43818af3679762f96cc40abbf06a5ca1ca807d29
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-11-30 23:10:07 -0300

    catch no-cep

--- src/api-request.js
+++ src/api-request.js
@@ -2,10 +2,11 @@ const axios = require('axios')
 
 exports.getFullAddressFromApi = async function(req, res) { 
     const { cep } = req.body;
+
+    if (!cep) return res.status(500).json({ erro: 'cep não informado'})
     
     const fetchedData = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
         .then(res => res)
-        .catch(e => res.send(e.message))
 
     // Na linha abaixo você deve colocar o BreakPoint e ler o que existe dentro de fetchedData e montar o fullAdress
     const fullAdress = ''
